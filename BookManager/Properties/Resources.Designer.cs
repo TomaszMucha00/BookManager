b7618922f89aec0ca730296af84285b912ed6ec1
Tomasz Mucha <TomaszMucha00@gmail.com>
2017-05-06 17:28:56 +0200

    Initial commit (second try)

--- BookManager/Properties/Resources.Designer.cs
+++ BookManager/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BookMenager.Properties
+namespace BookManager.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace BookMenager.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BookMenager.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BookManager.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- BookManager/Properties/Settings.Designer.cs
+++ BookManager/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BookMenager.Properties
+namespace BookManager.Properties
 {
 
 
